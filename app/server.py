5a64c83f7006afce886ca22054ad594da5e9f80f
Franklin Ribeiro <franklin.ribeiro@4linux.com.br>
2020-05-28 10:55:50 -0300

    Adicionando os files da api

---
+++ app/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify, render_template
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+db = create_engine('sqlite:///users.db')
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        conn = db.connect()
+        query = conn.execute("select * from users")
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+        return jsonify(result)
+
+api.add_resource(Users, '/users')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
